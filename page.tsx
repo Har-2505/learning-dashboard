b311ee8533c1d246b42cbab4238c073bc1d33722
Harshit Kumar Srivastav <your-email@example.com>
2026-05-28 21:31:26 +0530

    Created animated sidebar UI

--- page.tsx
+++ page.tsx
@@ -1,7 +1,19 @@
- export default function Home() {
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
   return (
-    <div>
-      <h1>Learning Dashboard</h1>
-    </div>
+    <main className="flex">
+      <Sidebar />
+
+      <div className="flex-1 p-10 bg-zinc-950 text-white min-h-screen">
+        <h1 className="text-4xl font-bold">
+          Learning Dashboard
+        </h1>
+
+        <p className="mt-4 text-zinc-400">
+          Welcome back, Harshit 👋
+        </p>
+      </div>
+    </main>
   );
 }
