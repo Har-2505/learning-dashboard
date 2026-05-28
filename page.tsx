95614494f23e2fb8f6f17d4e9fc72809f3b94f4d
Harshit Kumar Srivastav <your-email@example.com>
2026-05-28 22:44:36 +0530

    Added dashboard cards UI

--- page.tsx
+++ page.tsx
@@ -1,18 +1,21 @@
 import Sidebar from "@/components/Sidebar";
+import DashboardCards from "@/components/DashboardCards";
 
 export default function Home() {
   return (
     <main className="flex">
       <Sidebar />
 
-      <div className="flex-1 p-10 bg-zinc-950 text-white min-h-screen">
+      <div className="flex-1 bg-zinc-950 text-white min-h-screen p-8">
         <h1 className="text-4xl font-bold">
           Learning Dashboard
         </h1>
 
-        <p className="mt-4 text-zinc-400">
+        <p className="text-zinc-400 mt-2">
           Welcome back, Harshit 👋
         </p>
+
+        <DashboardCards />
       </div>
     </main>
   );
